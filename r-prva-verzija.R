506503001956750fa6ec4caf684ff898d300ce98
Marvin Herzog <marvin.herzog@hotmail.com>
2015-12-13 15:30:12 +0100

    nova tabelca očiščena

--- r-prva-verzija.R
+++ r-prva-verzija.R
@@ -5,13 +5,35 @@ require(ggplot2)
 
 
 CurUSD = read.csv(file = "podatki/SIPRI Milex data 1988-2014 Nov15 - Current USD.csv", sep = ";", dec = ".")
+#uredimo CurUSD
 
+
+names(CurUSD) <- lapply(CurUSD[5,],as.character)
+CurUSD[-c(1:5),] ->CurUSD
+CurUSD[-c(187:194),] ->CurUSD
+CurUSD[,-2] ->CurUSD
+
+for(i in 1:28){
+  CurUSD[,i] <- as.character(CurUSD[,i])
+  
+}
+filter(CurUSD,CurUSD$'1988'!="")->CurUSD
+CurUSD <- apply(CurUSD,2,function(x) gsub("xxx",NA,x))
+CurUSD <- apply(CurUSD,2,function(x) gsub("\\.\\s\\.",NA,x))
+
+for(i in 2:28){
+  CurUSD[,i] <- as.numeric(CurUSD[,i])
+  
+}
+
+View(CurUSD)
 
 
 podatki <- data.frame()
 NATOurl <- "https://en.wikipedia.org/wiki/Member_states_of_NATO"
 stran <- html_session(NATOurl) %>% read_html(encoding = "UTF-8")
 tabela <- stran %>% html_nodes(xpath ="//table[3]") %>% html_table() 
+
 
 
 #uredimo NATO
