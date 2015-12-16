169a4c044446379cae459cc1ebe133697274218e
Marvin Herzog <marvin.herzog@hotmail.com>
2015-12-16 12:11:45 +0100

    zemljevid v1 narejen

--- obdelava.R
+++ obdelava.R
@@ -41,11 +41,11 @@ test_data <- data.frame(
   var1 = 150 + c(0, cumsum(runif(49, -10, 10))),
   date = seq.Date(as.Date("2002-01-01"), by="1 month", length.out=100))
 
-obcine <- readShapeSpatial("podatki/mape/ne_110m_admin_0_countries.shp")
-testi <- merge(obcine, sel2)
+svet <- readShapeSpatial("podatki/mape/ne_110m_admin_0_countries.shp")
+#testi <- merge(svet, sel2)
 
 ptm <- proc.time()
-plot(obcine,xlim=c(-124.5, -115),ylim=c(15, 115))
+plot(svet,xlim=c(-124.5, -115),ylim=c(15, 115))
 proc.time() - ptm
 
 
@@ -53,3 +53,29 @@ proc.time() - ptm
 
 proc.time() - ptm
 
+pretvori.zemljevid <- function(zemljevid) {
+  fo <- fortify(zemljevid)
+  data <- zemljevid@data
+  data$id <- as.character(0:(nrow(data)-1))
+  return(inner_join(fo, data, by="id"))
+}
+
+hz <-pretvori.zemljevid(svet)
+
+zem <- ggplot() + geom_polygon(data = hz, aes(x=long, y=lat, group=group,fill=sovereignt))
+
+
+ptm <- proc.time()
+print(zem)
+proc.time() - ptm
+
+row.names(GDP)[c(140,82,52,112,16)] <- c("Russia","China", "Dominican Republic", "Bosnia and Herzegovina","Ivory Coast")
+row.names(GDP)[c(12,15,142,61,162)] <- c("Central African Republic", "Democratic Republic of the Congo", "Slovakia", "Trinidad and Tobogo","United Arab Emirates")
+row.names(GDP)[c(131,147,116,63,85,25)] <- c("Macedonia", "United Kingdom","Czech Republic","United States of America", "South Korea","Guinea Bissau")
+
+
+GDP[,"sovereignt"]<-row.names(GDP)
+zt <- merge(x=GDP,y=hz,all.y=TRUE)
+zt<- zt[with(zt, order(sovereignt, order)), ]
+zem <- ggplot() + geom_polygon(data = zt, aes(x=long, y=lat, group=group,fill=zt$"2014")) + scale_fill_gradient(low="#2412b4", high="#ff2a1a")
+print(zem)
